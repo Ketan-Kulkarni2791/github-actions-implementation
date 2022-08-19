d819e770c6d9ee1bef36667256c48f205ee6154a
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-19 15:28:00 +0530

    mypy hinting added

---
+++ infra/scripts/run_mypy.py
@@ -0,0 +1,19 @@
+"""Script for running mypy type hint checks."""
+from logging import RootLogger
+import subprocess
+
+from script_utils import get_path_for_file, ROOT_DIR
+
+
+def main() -> None:
+    """Invoke 'mypy' shell command."""
+    subprocess.run(
+        f"mypy {ROOT_DIR} --config-file {get_path_for_file('mypy.ini')}",
+        cwd=ROOT_DIR,
+        check=True,
+        shell=True
+    )
+    
+
+if __name__ == "__main__":
+    main()
