9cc2c9f0e3a15bc24689d0e736d1d7d3587943fb
Ketan Kulkarni <ketankulkarni2791@gmail.com>
2022-08-19 14:29:51 +0530

    prospector code testing

---
+++ infra/scripts/run_prospector.py
@@ -0,0 +1,16 @@
+"""Script for running Prospector checks on non-test files."""
+import subprocess
+
+from script_utils import get_path_for_file, ROOT_DIR
+
+def main() -> None:
+    """Invoke 'prospector' shell command on non-test files."""
+    subprocess.run(
+        f"prospector {ROOT_DIR} --profile {get_path_for_file('prospector.yml')}",
+        cwd=ROOT_DIR,
+        check=True,
+        shell=True
+    )
+    
+if __name__ == "__main__":
+    main()
---
+++ infra/scripts/script_utils.py
@@ -0,0 +1,29 @@
+"""Contains helper methods that the scripts will utilize to successfully build, test and deploy
+CDK application."""
+import logging
+import os
+from pathlib import Path
+from tkinter.tix import InputOnly
+
+ROOT_DIR = Path(__file__).parent.parent.parent
+# print(f"--------- root dir : {ROOT_DIR}")
+
+def get_path_for_file(input_file: str) -> bytes:
+    """Get relative path to input file."""
+    
+    for path, _, files in os.walk(ROOT_DIR):
+        for file in files:
+            if file == input_file:
+                return os.path.join(path, file)
+    logging.info("Could not find path to '%s' file.", input_file)
+    return b''
+
+def get_path_for_directory(input_dir: str) -> bytes:
+    """Get CDK directory path for script."""
+    
+    for path, dirs, _ in os.walk(ROOT_DIR):
+        for source_dir in dirs:
+            if 'cache' not in path and source_dir == input_dir:
+                return os.path.join(path, source_dir)
+    logging.info("Could not find path to '%s' directory.", input_dir)
+    return b''
